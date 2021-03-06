53529662cd4df2add80c5ce082126c0e8c5adb11
Li-Heng Hsu <yesleon@me.com>
2021-03-06 16:27:36 +0800

    optimize

--- Sources/SQLiteDatabase/RowDecoder.swift
+++ Sources/SQLiteDatabase/RowDecoder.swift
@@ -10,28 +10,33 @@ struct RowKeyedDecodingContainer<Key: CodingKey>: KeyedDecodingContainerProtocol
     
     let container: [String: () -> String?]
     
-    init(row: Row, codingPath: [CodingKey]) throws {
+    
+    init(row: Row) throws {
         var container = [String: () -> String?]()
+        var keys = [Key]()
         try row.forEachColumn { column in
             guard let name = column.getName() else { return }
+            guard let key = Key(stringValue: name) else { return }
             container[name] = column.getValue
+            keys.append(key)
         }
         self.container = container
-        self.codingPath = codingPath
+        self.allKeys = keys
     }
     
-    let codingPath: [CodingKey]
+    let codingPath: [CodingKey] = []
     
-    var allKeys: [Key] {
-        container.keys.compactMap(Key.init(stringValue:))
-    }
+    let allKeys: [Key]
     
     func contains(_ key: Key) -> Bool {
-        container.keys.contains(key.stringValue)
+        container[key.stringValue] != nil
     }
     
     func decodeNil(forKey key: Key) throws -> Bool {
-        container[key.stringValue] == nil
+        guard let getValue = container[key.stringValue] else {
+            throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "No value associated with key \(key.debugDescription)"))
+        }
+        return getValue() == nil
     }
     
     func decode<T: Decodable>(_ type: T.Type, forKey key: Key) throws -> T {
@@ -73,30 +78,29 @@ public final class RowDecoder: TopLevelDecoder {
     
     public typealias Input = Row
     
+    public init() { }
+    
     public func decode<T: Decodable>(_ type: T.Type, from row: Row) throws -> T {
-        let decoder = __RowDecoder(row: row, codingPath: [], userInfo: [:])
+        let decoder = __RowDecoder(row: row, userInfo: [:])
         return try T(from: decoder)
     }
-    
-    public init() { }
 }
 
 final class __RowDecoder: Decoder {
     
     let row: Row
     
-    let codingPath: [CodingKey]
+    let codingPath: [CodingKey] = []
     
     let userInfo: [CodingUserInfoKey : Any]
     
-    init(row: Row, codingPath: [CodingKey], userInfo: [CodingUserInfoKey: Any]) {
+    init(row: Row, userInfo: [CodingUserInfoKey: Any]) {
         self.row = row
-        self.codingPath = codingPath
         self.userInfo = userInfo
     }
     
     func container<Key: CodingKey>(keyedBy type: Key.Type) throws -> KeyedDecodingContainer<Key> {
-        let container = try RowKeyedDecodingContainer<Key>(row: row, codingPath: codingPath)
+        let container = try RowKeyedDecodingContainer<Key>(row: row)
         
         return KeyedDecodingContainer<Key>(container)
     }
